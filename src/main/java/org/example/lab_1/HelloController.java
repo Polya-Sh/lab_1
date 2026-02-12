d88cefad4641f4e1d83d198380795f7935f791e9
PolyaShinkaryova <shinkaryova.polina@yandex.ru>
2026-02-12 08:11:33 +0300

    Кнопка показа

--- src/main/java/org/example/lab_1/HelloController.java
+++ src/main/java/org/example/lab_1/HelloController.java
@@ -45,4 +45,32 @@ public class HelloController {
         cels.setText(String.valueOf(convert.get_cels()));
         kelv.setText(String.valueOf(convert.get_kelv()));
     }
+
+    public void click_output(ActionEvent actionEvent) {
+        try {
+            convert.set_kelv(Double.parseDouble(kelv.getText()));
+        } catch (NumberFormatException e) {
+            output.setText("Введите хотя бы одно значение");
+        }
+        try {
+            convert.set_cels(Double.parseDouble(cels.getText()));
+        } catch (NumberFormatException e) {
+            output.setText("Введите хотя бы одно значение");
+        }
+        try {
+            convert.set_farng(Double.parseDouble(farng.getText()));
+        } catch (NumberFormatException e) {
+            output.setText("Введите хотя бы одно значение");
+        }
+        ;
+        if (convert.get_cels() <= -20) {
+            output.setText("Холодно");
+        } else if (convert.get_cels()>-20 && convert.get_cels()<10){
+            output.setText("Прохладно");
+        } else if (convert.get_cels()>=10 && convert.get_cels()<=20) {
+            output.setText("Нормально");
+        } else if (convert.get_cels() > 20){
+            output.setText("Жарко");
+        }
+    }
 }
