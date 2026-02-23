93d272cd8d146e4793cd01f5f06a5f4a60617d5f
PolyaShinkaryova <shinkaryova.polina@yandex.ru>
2026-02-23 16:52:49 +0300

    не работает валидация

--- src/main/java/org/example/lab_1/HelloController.java
+++ src/main/java/org/example/lab_1/HelloController.java
@@ -1,6 +1,5 @@
 package org.example.lab_1;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,32 +22,41 @@ public class HelloController implements Initializable {
     @FXML
     private TextField farng;
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        convert.set_cels(0.0);
+        convert.set_kelv(273.15);
+        convert.set_farng(32.0);
+
         cels.textProperty().bindBidirectional(convert._celsProperty(), new NumberStringConverter());
         kelv.textProperty().bindBidirectional(convert._kelvProperty(), new NumberStringConverter());
         farng.textProperty().bindBidirectional(convert._farngProperty(), new NumberStringConverter());
     }
 
     public void click_output(ActionEvent actionEvent) {
+
         try {
-            double celsius = convert.get_cels();
+            double celsius = Double.parseDouble(cels.getText());
 
             if (celsius <= -20) {
                 output.setText("Холодно");
                 output.setStyle("-fx-background-color: blue;");
-            } else if (celsius > -20 && celsius < 10) {
+            } else if (celsius < 10 && celsius > -20) {
                 output.setText("Прохладно");
                 output.setStyle("-fx-background-color: lightblue;");
-            } else if (celsius >= 10 && celsius <= 20) {
+            } else if (celsius <= 20 && celsius > 10) {
                 output.setText("Нормально");
                 output.setStyle("-fx-background-color: lightgreen;");
-            } else if (celsius > 20) {
+            } else {
                 output.setText("Жарко");
-                output.setStyle("-fx-background-color: lightred;");
+                output.setStyle("-fx-background-color: red;");
             }
-        } catch (Exception e) {
-            output.setText("Ошибка");
+
+        } catch (NumberFormatException e) {
+            output.setText("Некорректный ввод");
+            output.setStyle("-fx-background-color: gray;");
         }
     }
-}
+    }
